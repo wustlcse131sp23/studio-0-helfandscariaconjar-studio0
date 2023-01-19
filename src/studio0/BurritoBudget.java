d660feead25a07714b0d222a6b219490a20c2575
zachhelfand <zachhelfand@10.232.150.249>
2023-01-19 15:09:46 -0600

    fixed code

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -8,12 +8,14 @@ public class BurritoBudget {
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
 		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+		double totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
 		double averageWeeklyBurritos = totalBurritos / 4;
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
-		
+		averageWeeklyPrice = Math.round(averageWeeklyPrice*100);
+		averageWeeklyPrice = averageWeeklyPrice / 100;
+
 		System.out.println("You have spent an average of $" + averageWeeklyPrice + " per week on burritos.");
 	
 	}
